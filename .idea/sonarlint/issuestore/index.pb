98066972263c5b52841c06ebcba1988417632d40
edik <hardrockallely@mail.ru>
2021-02-22 11:29:23 +0300

    First

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+;
+Runner.java,c\f\cfcd80c2266a00187b8181f6c920979d469cc42c
---
+++ by/gsu/pms/Manager.java
@@ -0,0 +1,70 @@
+package by.gsu.pms;
+
+public class Manager {
+
+
+    private String perName;
+    private int perAge;
+    private boolean perDuties;
+
+    //constructors
+
+    public Manager() {
+        this("",0,false);
+    }
+    public Manager(String perName, int perAge, boolean perDuties) {
+        super();
+        this.perName = perName;
+        this.perAge = perAge;
+        this.perDuties = perDuties;
+    }
+
+    //getters and setters
+
+    public String getPerName() {
+        return perName;
+    }
+
+    public void setPerName(String perName) {
+        this.perName = perName;
+    }
+
+    public int getPerAge() {
+        return perAge;
+    }
+
+    public void setPerAge(int perAge) {
+        this.perAge = perAge;
+    }
+
+    public boolean isPerDuties() {
+        return perDuties;
+    }
+
+    public void setPerDuties(boolean perDuties) {
+        this.perDuties = perDuties;
+    }
+
+
+    //methods
+
+    public int getTotal() {
+        return RATE * days + transport;
+    }
+
+    public void show() {
+
+        System.out.println("rate = " + RATE);
+        System.out.println("account = " + account);
+        System.out.println("transport = " + transport);
+        System.out.println("days = " + days);
+
+    }
+
+    @Override
+    public String toString() {
+        return RATE + ";" + account + ";" + transport + ";" + days + ";" + getTotal();
+    }
+
+
+}
