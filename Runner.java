bee69d054475da787d248c2027ae9a81b9f91e8b
GPinchukov <pinchukov000@gmail.com>
2021-02-28 17:15:49 +0300

    final_comm

--- Runner.java
+++ Runner.java
@@ -5,7 +5,7 @@ public class Runner {
     public static void main(String[] args) {
 
 
-        Manager[] manager = {
+        Manager[] managers = {
 
                 new Manager("Sakovich", 45, true),
                 null,
@@ -33,58 +33,10 @@ public class Runner {
             }
         }
 
-        /*
-         *3) Change the employee`s transportaion expenses for the last object of the array.
-         */
 
-        employees[6].setTransport(700);
-        System.out.println(employees[6]);
-
-        /*
-         *4) Print the duration of two initial business trips by the single operator.
-         * Example:
-         * Duration = 9
-         */
-
-        System.out.println("Duration = "+(employees[1].getDays()+employees[0].getDays()));
-
-        /*
-         *5) Print the array content to the console (one element per line), using toString( ) method implicitly.
-         */
-
-        for (EmployeeExpenses employee : employees) {
-            System.out.println(employee);
-        }
-
-        /*
-         *6) Find the sum of total expenses.
-         */
-
-        int totalExpenses = 0;
-
-        for (EmployeeExpenses employee : employees) {
-            if (employee != null) {
-                totalExpenses += employee.getTotal();
-            }
-        }
-
-        System.out.println("Total expenses = " + totalExpenses);
-
-        /*
-         * 7) Find and print account name employee’s with maximum total expenses.
-         */
-
-        int max = 0;
-        for (EmployeeExpenses employee : employees) {
-            if (employee != null && employee.getTotal() > max) {
-                max = employee.getTotal();
-            }
-        }
-        System.out.println("Max = " + max);
-
-        Converter Rus = new Converter(33,2);
-        System.out.println("Conv = " + Rus.convert(employees[3].RATE));
-
-
+        int averagePerAge = totalPerAge/ managers.length;
+        System.out.println("Average age = " + averagePerAge);
+        System.out.println("Workers in charge = " + totalPerDuties);
     }
 }
+
