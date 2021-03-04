1153d9f2175f6f40bcd8ee4a24617cd6694cd2fd
GPinchukov <pinchukov000@gmail.com>
2021-03-04 11:10:47 +0300

    init: start task

---
+++ by/gsu/asoilab/Material.java
@@ -0,0 +1,18 @@
+package by.gsu.asoilab;
+
+public class Material {
+
+
+    private String Name;
+    private int Density;
+
+    //constructors
+
+    public Material() {
+        this("",0);
+    }
+    public Material(String name, int density) {
+        super();
+        this.name = perName;
+        this.density = Density;
+    }
